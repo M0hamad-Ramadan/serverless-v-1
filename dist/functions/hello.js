b768c74618d162f6120cef01ddd2a91ea0d8a45c
mohammed <moaboelyazied@gmail.com>
2024-01-30 10:01:36 +0200

    get geo data

--- dist/functions/hello.js
+++ dist/functions/hello.js
@@ -1,6 +1,11 @@
-exports.handler = async () => {
+export async function handler() {
+
+  let url = 'https://geolocation-db.com/json/';
+  let res = await fetch(url)
+  let geoData = await res.json();
+  
   return {
     statusCode: 200,
-    body: 'Hello world!',
+    body: geoData,
   };
-};
+}
